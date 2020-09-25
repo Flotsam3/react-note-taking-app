272cead4740e57a90648bb0ab3a081bc91d103b3
Joker <admin@e.mail.de>
2020-09-25 18:56:03 +0200

    Updates

---
+++ precache-manifest.cd62eba6eb9f393fd9c2e0c94d0e1c8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8695e4ba2f59a92def4bb64399c921c3",
+    "url": "/windows/index.html"
+  },
+  {
+    "revision": "a3ddd874374a424900ec",
+    "url": "/windows/static/css/main.bca758f4.chunk.css"
+  },
+  {
+    "revision": "d544eab7e7b8c50b2db2",
+    "url": "/windows/static/js/2.4ed6ca00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/windows/static/js/2.4ed6ca00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3ddd874374a424900ec",
+    "url": "/windows/static/js/main.a56a9ad8.chunk.js"
+  },
+  {
+    "revision": "e4a0b22f8410c6d00555",
+    "url": "/windows/static/js/runtime-main.f1266f7d.js"
+  }
+]);
